3d2e83c9d29b2324369abd2b2146f26dff2b284b
Minimow <julien.bergeron9@gmail.com>
2015-10-27 16:23:48 -0400

    La page de combat utilise la table de correspondance. Elle n'exécute qu'une seule round parce que nous n'avons pas les points d'endurances initiaux.

--- TP1/Le loup solitaire/Battle.js
+++ TP1/Le loup solitaire/Battle.js
@@ -7,36 +7,64 @@ function Battle() {
     this.ennemyHabilities = 0;
 }
 
-Battle.prototype.fight = function() {
+Battle.prototype.fight = function () {
 
     function BattleResult() {
-        this.AttackRatio = 0;
+        this.combatRatio = 0;
         this.randomNumber = 0;
         this.playerEndurance = 0;
         this.playerEnduranceDiff = 0;
         this.ennemyEndurance = 0;
         this.ennemyEnduranceDiff = 0;
     }
-    
-    var attackRatio = this.playerHabilities - this.ennemyHabilities;
+
+    var combatRatio = this.playerHabilities - this.ennemyHabilities;
     var randomNumber = common_functions.getRandom(0, 9);
 
-    // En attendant d'avoir le tableau de correspondance.
-    var playerLoss = battleTable[0][0];
-    var ennemyLoss = battleTable[2][1];
+    var tableResult;
+
+    if(combatRatio > 0){
+        tableResult = combatRatioPos[randomNumber][Math.abs(combatRatio)];
+  	} else{
+        tableResult = combatRatioNeg[randomNumber][Math.abs(combatRatio)];
+    }
 
     var result = new BattleResult();
-    result.AttackRatio = attackRatio;
+    result.combatRatio = combatRatio;
     result.randomNumber = randomNumber;
     result.playerEndurance = this.playerEndurance;
-    result.playerEnduranceDiff = playerLoss;
+    result.playerEnduranceDiff = tableResult[0];
     result.ennemyEndurance = this.ennemyEndurance;
-    result.ennemyEnduranceDiff = ennemyLoss;
+    result.ennemyEnduranceDiff = tableResult[1];
 
 
     return result;
 }
 
-var battleTable = [[10,11], [12,13], [3, 4], [5,1]];
+var combatRatioNeg = [
+[[12,0],[11,0],[11,0],[10,0],[10,0],[9,0],[9,0],[8,0],[8,0],[7,0],[7,0],[6,0]],
+[[3,5],[2,5],[2,5],[1,6],[1,6],[0,6],[0,6],[0,8],[0,8],[0,-1],[0,-1],[0,-1]],
+[[4,4],[3,5],[3,5],[2,5],[2,5],[1,6],[1,6],[0,7],[0,7],[0,8],[0,8], [0,-1]],
+[[5,4],[4,4],[4,4],[3,5],[3,5],[2,5],[2,5],[1,6],[1,6],[0,7],[0,7],[0,8]],
+[[6,3],[5,4],[5,4],[4,4],[4,4],[3,5],[3,5],[2,6],[2,6],[1,7],[1,7],[0,8]],
+[[7,2],[6,3],[6,3],[5,4],[5,4],[4,4],[4,4],[3,5],[3,5],[2,6],[2,6],[1,7]],
+[[8,2],[7,2],[7,2],[6,3],[6,3],[5,4],[5,4],[4,5],[4,5],[3,6],[3,6],[2,6]],
+[[9,1],[8,2],[8,2],[7,2],[7,2],[6,3],[6,3],[5,4],[5,4],[4,5],[4,5],[3,5]],
+[[10,0],[9,1],[9,1],[8,1],[8,1],[7,2],[7,2],[6,3],[6,3],[5,4],[5,4],[4,4]],
+[[11,0],[10,0],[10,0],[9,0],[9,0],[8,0],[8,0],[7,2],[7,2],[6,3],[6,3],[5,3]]
+];
+
+var combatRatioPos = [
+[[12,0],[14,0],[14,0],[15,0],[15,0],[18,0],[18,0],[-1,0],[-1,0],[-1,0],[-1,0],[-1,0]],
+[[3,5], [4,5], [4,5], [5,4] ,[5,4] ,[6,4] ,[6,4] ,[7,4] ,[7,4] ,[8,3] ,[8,3] ,[9,3] ],
+[[4,4], [5,4], [5,4], [6,3] ,[6,3] ,[7,3] ,[7,3] ,[8,3] ,[8,3] ,[9,3] ,[9,3] ,[10,2]],
+[[5,4], [6,3], [6,3], [7,3] ,[7,3] ,[8,3] ,[8,3] ,[9,2] ,[9,2] ,[10,2] ,[10,2] ,[11,2]],
+[[6,3], [7,3], [7,3], [8,2] ,[8,2] ,[9,2] ,[9,2] ,[10,2],[10,2],[11,2] ,[11,2] ,[12,2]],
+[[7,2], [8,2], [8,2], [9,2] ,[9,2] ,[10,2],[10,2],[11,2],[11,2],[12,2] ,[12,2] ,[14,1]],
+[[8,2], [9,2], [9,2], [10,2],[10,2],[11,1],[11,1],[12,1],[12,1],[14,1] ,[14,1] ,[16,1]],
+[[9,1], [10,1],[10,1],[11,1],[11,1],[12,0],[12,0],[12,0],[12,0],[16,0] ,[16,0] ,[18,0]],
+[[10,0],[11,0],[11,0],[12,0],[12,0],[14,0],[14,0],[14,0],[14,0],[18,0] ,[18,0] ,[-1,0]],
+[[11,0],[12,0],[12,0],[14,0],[14,0],[16,0],[16,0],[16,0],[16,0],[-1,0],[-1,0],[-1,0]]
+];
 
 module.exports = Battle;
